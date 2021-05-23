b9b6663b17815cd887e1810c9e5032cca521cc3b
MohammedHassan98 <mohammedhassanedofficaial@gmail.com>
2021-05-23 09:32:19 +0200

    clean uo app.js file

--- Backend/app.js
+++ Backend/app.js
@@ -17,10 +17,6 @@ const Book = require('./models/Book')
 const Admin = require('./models/Admin')
 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,7 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -46,6 +41,7 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
+// Connect Database
 DB.sync({ force: true })
   .then(() => {
     console.log("Database is connected")
