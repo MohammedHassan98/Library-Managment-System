b655aecacf3482890909c29e43c923cc90e4186c
MohammedHassan98 <mohammedhassanedofficial@gmail.com>
2021-05-28 10:54:10 +0200

    fix footer styling

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
+import Facebook from '../Assets/icons/facebook.svg'
+import Instagram from '../Assets/icons/instagram-logo.svg'
+import Twitter from '../Assets/icons/twitter.svg'
 
 export default function Footer() {
     return (
         <footer>
             <div className="footerSection">
                 <div className="row">
-                    <div className="col-3">
-                        <h4>Library</h4>
+                    <div className="col-2">
+                        <h4 style={{ 'padding': '10px' }}>Library</h4>
                     </div>
 
-                    <div className="col-6">
+                    <div className="col-4">
                         <ul>
-                            <li>Help Center</li>
-                            <li>Contact Us</li>
-                            <li>About Us</li>
+                            <li> <a href='#' className="links" >Help Center </a></li>
+                            <li> <a href='#' className="links" >Contact Us </a></li>
+                            <li> <a href='#' className="links" >About Us </a></li>
                         </ul>
                     </div>
-                    <div className="col-3" style={{'padding': '10px'}}>
-                        #Library
+                    <div className="col-3" style={{'display': 'inline-block'}}>
+                        <ul>
+                            <li> <a href='#' ><img src={Facebook} alt="faceIcon" className="SocialIcon"/> </a></li>
+                            <li> <a href='#' ><img src={Instagram} alt="instaIcon" className="SocialIcon" /> </a></li>
+                            <li> <a href='#' ><img src={Twitter} alt="twitterIcon" className="SocialIcon" /> </a></li>
+                            <li style={{marginLeft:'-10px'}}>#Library</li>
+                        </ul>
                     </div>
                 </div>
             </div>
--- Frontend/src/components/HomePage.jsx
+++ Frontend/src/components/HomePage.jsx
@@ -5,7 +5,7 @@ import Footer from './Footer'
 export default class HomePage extends Component {
     render() {
         return (
-            <div className="container-fluid" style={{'margin':'0', 'width':'100%'}}>
+            <div className="container-fluid" style={{'margin':'0', 'width':'100%', 'padding': '0'}}>
                 <div className="row">
                     <NavBar />
                 </div>
