4365237a365ab42bd9ea06f29ddd4e09c619d52e
MohammedHassan98 <mohammedhassanedofficial@gmail.com>
2021-05-28 18:46:47 +0200

    Some Card Styling + fetch images from DB

--- Frontend/src/components/Card.jsx
+++ Frontend/src/components/Card.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import book from '../Assets/Images/download.jpeg'
 import { Link } from 'react-router-dom'
 
 export default function Card(props) {
     return (
-        <div className="card bookCard" key={props.book.BookId}>
-            <img className="card-img-top bookImg" src={book} alt="bookImage" />
+        <div className="card bookCard" key={props.book.BookId} style={{borderRadius:'18px'}}>
+            <img className="card-img-top bookImg" style={{borderRadius: '18px'}} src={`http://localhost:5000/${props.book.CoverImageUrl}`} alt="bookImage" />
             <div className="card-body">
                 <h5 className="card-title bookName">
                     <Link className="bookName" to={{
@@ -15,43 +14,11 @@ export default function Card(props) {
                     </Link>
                 </h5>
                 <h5 className="card-subtitle mb-2 text-muted bookAuthor">by {props.book.BookAuthor}</h5>
-                <p className="card-text bookDesc">{props.book.Description}</p>
+                <p className="card-text bookDesc">{props.book.ShortDescription}</p>
             </div>
         </div>
     )
 }
-
-
-    // editBook = () => {
-    //     console.log('test')
-    // }
-
-
-    // addBook = () => { }
-
-
-                // {/* <div className="card-footer bg-transparent">
-                //     <ul>
-                //         <li>
-                //             <button className="btn" onClick={this.editBook}>
-                //                 <img src={edit} alt="editButton" />
-                //             </button>
-                //         </li>
-
-                //         <li>
-                //             <button className="btn" onClick={this.deleteBook}>
-                //                 <img src={del} alt="delButton" />
-                //             </button>
-                //         </li>
-
-                //         <li>
-                //             <button className="btn" onClick={this.addBook}>
-                //                 <img src={sell} alt="sellButton" />
-                //             </button>
-                //         </li>
-                //     </ul>
-                // </div> */}
-
 
 // BookAuthor: "Mohammed Hassan"
 // BookId: 4
